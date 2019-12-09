5de64beb99e82b9ab2c9c6f30c171ea343ddf272
Colin Maudry <colin@maudry.com>
2019-12-09 11:32:21 +0100

    Main script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-jq -f options.jq ~/git/decp-rama/json/decp.ocds.json > options.json
+if [[ -z $1 ]]
+then
+    echo "You must specify the path to the data file in \$1."
+    exit 1
+else
+    jsonpath=$1
+    if [[ "${jsonpath:0:4}" == "http" ]]
+    then
+        curl -Lq $jsonpath -o "_temp.json"
+        jsonpath="./_temp.json"
+    fi
+fi
 
-pug -O options.json index.pug
+if [[ -z $2 ]]
+then
+    lang="en"
+else
+    lang=$2
+fi
+
+jq -f options.jq --slurpfile strings strings.json --arg lang $lang $jsonpath > options.json
+
+pug -P -O options.json index.pug
+
+rm -f _temp.json
